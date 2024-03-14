fb2cb0b960ebf934148c3834b27fa584f0211dd3
Seongho Lee <seongholee4@gmail.com>
2024-03-14 01:54:06 -0400

    final code

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// Your goal is create components, create props and mapping data to the components.
+// Notice the emojipedia.js file in the directory.
+// 1. Create Entry component
+// 2. Entry Component will need props to replaces hard coded data in App.jsx
+//3. Map through the emojipedia array and render Entry components. Don’t forget to import.
